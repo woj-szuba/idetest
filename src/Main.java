c93438e2333b02932e8f0b667bf7176d574a7494
Wojciech Szuba <woj.szuba@gmail.com>
2018-12-04 15:00:28 +0100

    Added string to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        for(int i = 0; i <= "Hello World!".length(); i++)
+        {
+            System.out.print("=");
+        }
+
     }
 }
